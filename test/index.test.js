cce876ced05e4143997d5a6f76bdbaa203bddfd0
Ganesha <reekoheek@gmail.com>
2018-09-08 16:43:07 +0700

    Work with twlv core v1

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 const { Node } = require('@twlv/core');
-const { TcpListener } = require('@twlv/core/transports/tcp');
+const { TcpReceiver } = require('@twlv/core/transports/tcp');
 const { MDNSFinder } = require('../');
 const assert = require('assert');
 
@@ -17,8 +17,8 @@ describe('MDNSFinder', () => {
       let node1 = new Node();
       let node2 = new Node();
 
-      node1.addListener(new TcpListener());
-      node2.addListener(new TcpListener());
+      node1.addReceiver(new TcpReceiver());
+      node2.addReceiver(new TcpReceiver());
 
       node1.addFinder(new MDNSFinder());
       node2.addFinder(new MDNSFinder());
@@ -28,9 +28,9 @@ describe('MDNSFinder', () => {
         await node2.start();
 
         let peer = await node1.find(node2.identity.address);
-        assert.equal(peer.address, node2.advertisement.address);
-        assert.equal(peer.pubKey, node2.advertisement.pubKey);
-        assert.equal(peer.urls[0], node2.advertisement.urls[0]);
+        assert.strictEqual(peer.address, node2.advertisement.address);
+        assert.strictEqual(peer.pubKey, node2.advertisement.pubKey);
+        assert.strictEqual(peer.urls[0], node2.advertisement.urls[0]);
       } finally {
         await node1.stop();
         await node2.stop();
